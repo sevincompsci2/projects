3648057ca1771d0ff81c18662afb97156e2bd2c4
sevincompsci2 <moran_sevin@students.columbusstate.edu>
2026-08-24 18:52:16 -0400

    Add files via upload
    Assignment 1

---
+++ Module1Assignment1.java
@@ -0,0 +1,14 @@
+//Name: Sevin Moran
+
+public class Module1Assignment1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundo");
+        System.out.println("Preferred job title: Craft and Fine Artist");
+        System.out.println("Why: I enjoy being creative and making art.");
+        System.out.println("Median income: $56,260");
+    }
+}
+
+
